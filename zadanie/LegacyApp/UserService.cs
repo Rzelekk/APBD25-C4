a61fc1cc5b662c7c9b3488671bd1bacf58975d72
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-26 17:04:07 +0100

    Refactor if to CheckCreditLimit method

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -16,33 +16,7 @@ namespace LegacyApp
 
             CheckClientImportanceAndSetupCreditsLimits(client, user);
 
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
-
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
+            if (!ChceckCreditLimit(user)) return false;
 
             UserDataAccess.AddUser(user);
             return true;
