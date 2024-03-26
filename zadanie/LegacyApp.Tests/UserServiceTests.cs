a9f03875a5a4a3a35f45d5a81a55f85c3d642054
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-26 17:04:42 +0100

    change in test AddUser_ReturnsFalseWhenFirstNameIsEmpty

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -2,21 +2,26 @@ namespace LegacyApp.Tests;
 
 public class UserServiceTests
 {
+    
+    //AddUser_ReturnsFalseWhenFirstNameIsEmpty
     [Fact]
     public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
+        
         // Arrange
         var userService = new UserService();
-        
+
         // Act
         var result = userService.AddUser(
-            null,
-            "Kowalski",
-            "kowalski@kowal.com",
+            null, 
+            "Kowalski", 
+            "kowalski@kowalski.pl",
             DateTime.Parse("2000-01-01"),
             1
-            );
+        );
+
         // Assert
+        
         Assert.False(result);
     }
     
