97509a95253c5db2ff4cfdf0a5405ca6e92d5cdd
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-03-26 16:55:31 +0100

    Add ChceckClientImportanceAndSetupCreditsLimits method impelemntation

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -71,6 +71,32 @@ namespace LegacyApp
 
      
 
+        private void CheckClientImportanceAndSetupCreditsLimits(Client client, User user)
+        {
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+        }
+
         private bool CheckIsAgeOver21(DateTime dateOfBirth)
         {
             var now = DateTime.Now;
